6159e12b1ad3a63b027b9d536b9780b49fb2307c
Rachel Jackson <racheljackson@Rachels-MacBook-Pro-2.local>
2016-05-29 00:24:06 -0400

    index and new user router created and working - tested via postman

--- restaurant_roulette_app/controllers/users.js
+++ restaurant_roulette_app/controllers/users.js
@@ -4,10 +4,12 @@
 var express = require('express');
 var router = express();
 var request = require('request');
+var cookieParser = require('cookie-parser');
 var Users = require('../models/users.js');
 
 
-
+//index page
+//===================================
 router.get('/', function(req, res) {
 	console.log("=======>index router is working<=====");
 	Users.find({}, function(err, users) {
@@ -15,37 +17,13 @@ router.get('/', function(req, res) {
 	});
 });
 
-//CREATE
-// router.post('/createUser', function(req, res){
-// 	//grab user data from forms
-// 	var userData = req.body;
-// 	//grab new User object with req.body data
-// 	var user = new Users(req.body);
-// 	//create new User and redirect to root page
-// 	Users.create(userData, function(err, user){
-//  		res.cookie('name', user.name);
-//  		res.redirect('/films/');
-// 	});
-// });
+//Get Create-User Page
+//====================================
+router.get('/newuser', function(req, res) {
+	console.log("=======>Newbie On The Way<======");
+	res.render('createuser.ejs');
+});
 
-// To hit the OMDB API, we make a get request:
 
-//   // show
-//  router.get('/movies/:title',function(req,res){
-//  //grab movie title from form
-//  var title = req.params.title;
-// //create new variable for movie
-// var myMovie;
-// //GET REQUEST to OMDB API, using title
-// request('http://www.omdbapi.com/?t=' + title, function(error, response, body){
-//   //if this request IS SUCCESSFUL
-//   if(!error && response.statusCode == 200){
-//   //save JSON object to myMovie variable
-//   myMovie = JSON.parse(body);
-//   //display show page using this data
-//   res.render('show.ejs', {movie: myMovie});
-//  }
-//   })
-// });
 
 module.exports = router;
